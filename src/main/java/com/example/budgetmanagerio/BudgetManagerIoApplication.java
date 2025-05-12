52ac7642d2c0b2596812c3fe121b1df702a7524b
simodev744 <ammar.mohamed744@gmail.com>
2025-05-12 13:03:18 +0200

    first commit

---
+++ src/main/java/com/example/budgetmanagerio/BudgetManagerIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetmanagerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagerIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagerIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budgetmanagerio/BudgetManagerIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetmanagerio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
