2d46a639a42b1496bf0f54f4da19adfaeee9d3fd
iliass sidqi <enaailias@gmail.com>
2025-05-15 12:14:54 +0100

    budget model

---
+++ src/main/java/com/example/budgetmanagerio/model/Budget.java
@@ -0,0 +1,41 @@
+package com.example.budgetmanagerio.model;
+
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "budgets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+
+}
